bbfd5689807bfec85c0a806b363c3fb901a2999a
Ovidiu Petridean <opetridean@sdl.com>
2020-02-22 13:22:11 +0200

    new examples from lecture 12

---
+++ src/main/java/org/ucode/lecture12/examples/Car.java
@@ -0,0 +1,12 @@
+package lesson12;
+
+public class Car extends Vehicle {
+    public void accelerate() {
+        System.out.println("Car accelerating");
+    }
+
+    public void slowDown() {
+        System.out.println("Car slowing down");
+    }
+
+}
